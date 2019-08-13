d2c1b3c540a8dcca571b8250db90c6a6c4fcd898
GolThr <1344006993@qq.com>
2019-08-13 17:40:36 +0800

    GolThr(8013)

---
+++ app/src/main/java/com/golthr/travelguide/obj/Inform.java
@@ -0,0 +1,33 @@
+package com.golthr.travelguide.obj;
+
+import cn.bmob.v3.BmobUser;
+
+public class Inform extends BmobUser {
+    private String Name;
+    private String Address;
+    private String Sex;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getSex() {
+        return Sex;
+    }
+
+    public void setSex(String sex) {
+        Sex = sex;
+    }
+}
